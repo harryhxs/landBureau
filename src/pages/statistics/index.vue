2ae1c1fba4ce47aaa1e623f5e58ac07eea146d98
harrhe <harrhe@deloitte.com.cn>
2019-12-16 10:08:25 +0800

    统计页面

--- src/pages/statistics/index.vue
+++ src/pages/statistics/index.vue
@@ -1,6 +1,25 @@
 <!-- 统计信息 -->
 <template>
   <div class="statistic-container">
+    <van-cell-group>
+      <van-field
+        :value="startTime"
+        title-width="100px"
+        required
+        label="开始时间"
+        placeholder="请选择开始时间"
+        @change="onChange($event, 'start')"
+      />
+      <van-field
+        :value="endTime"
+        title-width="100px"
+        required
+        label="结束时间"
+        disabled
+        :placeholder="'请选择结束使劲'"
+        @click="openDatePicker('end')"
+      />
+    </van-cell-group>
     <div class="title">{{ userName }}的巡查统计情况</div>
     <scroll-view
       class="index-page-scroll-view"
@@ -15,6 +34,19 @@
         />
       </template>
     </scroll-view>
+    <!-- 时间插件 -->
+    <van-action-sheet
+      :show="showPicker"
+      title="选择日期"
+      @close="showPicker = false"
+    >
+      <van-datetime-picker
+        type="date"
+        :value="currentDate"
+        @confirm="changeDate"
+        @cancel="showPicker = false"
+      />
+    </van-action-sheet>
   </div>
 </template>
 
@@ -30,7 +62,12 @@ export default {
   data() {
     return {
       userName: '张大大',
-      listData: [{}, {}]
+      listData: [{}, {}],
+      showPicker: false,
+      currentType: '',
+      currentDate: new Date().getTime(),
+      startTime: '',
+      endTime: ''
     }
   },
   computed: {
@@ -39,7 +76,7 @@ export default {
   watch: {
 
   },
-  created() {
+  onShow() {
 
   },
   methods: {
@@ -48,6 +85,37 @@ export default {
     },
     scrollContainerFn() {
 
+    },
+    openDatePicker(type) {
+      this.showPicker = true
+      this.currentType = type
+    },
+    changeDate(event) {
+      this.currentDate = event.mp.detail
+      if (this.currentType == 'start') {
+        this.startTime = event.mp.detail
+      } else {
+        this.endTime = event.mp.detail
+      }
+      this.getData()
+    },
+    getData() {
+      wx.showLoading({
+        title: '加载中',
+        mask: true
+      })
+      this.$http.post({
+        url: '/statistics/user',
+        data: {
+          startTime: this.startTime,
+          endTime: this.endTime
+        }
+      }).then(res => {
+        if (res && res.data) {
+          this.listData = res.data || []
+        }
+        wx.hideLoading()
+      })
     }
   }
 }
